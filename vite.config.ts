a96bc05ceca5bb2f1df6cb582d139188f2f18353
An Nguyễn <ntanan1996@gmail.com>
2022-06-01 11:02:45 +0700

    add env test

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,32 @@
 import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      ['/api/v1']: {
-        target: 'http://localhost:3000',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+  const isReport = mode === 'report'
+
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  console.log('mode', mode)
+  console.log('env', env)
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
-  },
+    },
+    server: {
+      proxy: {
+        ['/api/v1']: {
+          target: env.VITE_BASE_API || 'http://localhost:3000',
+          changeOrigin: true
+        }
+      }
+    },
+  }
 })
